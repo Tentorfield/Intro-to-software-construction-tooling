fbfc590d1e3491a9e2de9a8f98c232025945b1f5
Tentorfield <hngo017@ucr.edu>
2021-10-04 10:30:14 -0700

    New update for test.cpp for gtest cases

--- test.cpp
+++ test.cpp
@@ -6,6 +6,25 @@ TEST(EchoTest, HelloWorld) {
     char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "hello"; test_val[2] = "world";
     EXPECT_EQ("hello world", echo(3,test_val));
 }
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+TEST(EchoTest, WhiteSpace) {
+    char* test_val[2]; test_val[0] = "./c-echo";test_val[1] = " ";
+    EXPECT_EQ(" ", echo(2,test_val));
+}
+
+TEST(EchoTest, NewLine) {
+    char* test_val[2]; test_val[0] = "./c-echo";test_val[1] = "\n";
+    EXPECT_EQ("\n", echo(2,test_val));
+}
+
+TEST(EchoTest, NonStrings) {
+    char* test_val[2]; test_val[0] = "./c-echo";test_val[1] = "!@#$%^&*";
+    EXPECT_EQ("", echo(1,test_val));
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
